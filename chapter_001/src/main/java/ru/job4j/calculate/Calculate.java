0fa36e9c0c8fe33f731a7658ba095a553dc68822
Petr Arsentev <lemonchic1@gmail.com>
2019-01-17 20:23:14 +0300

    My

---
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -0,0 +1,15 @@
+package ru.job4j.calculate;
+/**
+*Calculate
+*@author Alexey Bogutsky
+*/
+public class Calculate {
+/** Method main.
+* @param args - содержит параметры командной строки.
+* @return - ничего не возвращает
+*/
+   public static void main (String[] args){
+        System.out.println("Hello World");
+
+    }
+}
