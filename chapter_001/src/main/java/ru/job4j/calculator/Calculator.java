2b9b179497f4f22ef4b3279fce01c1743d8d5432
Petr Arsentev <lemonchic1@gmail.com>
2019-02-11 21:06:28 +0300

    3.1. Элементарный калькулятор. [ #100506](new)

--- chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
+++ chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
@@ -1,23 +1,38 @@
 package ru.job4j.calculator;
-
+/**
+ *Calculate
+ *@author Alexey Bogutsky
+ */
 public class Calculator {
     private double result;
     public double getResult() {
         return this.result;
     }
-
+/** Method add
+ * @param first - first operand.
+ * @param second - second operand
+ */
     public void add(double first, double second) {
         this.result = first + second;
     }
-
+    /** Method substruct
+     * @param first - first operand.
+     * @param second - second operand
+     */
     public void substract(double first, double second) {
         this.result = first - second;
     }
-
+    /** Method multiply
+     * @param first - first operand.
+     * @param second - second operand
+     */
     public void multiply(double first, double second) {
         this.result = first * second;
     }
-
+    /** Method divide
+     * @param first - first operand.
+     * @param second - second operand
+     */
     public void divide(double first, double second) {
         this.result = first / second;
     }
---
+++ chapter_001/src/main/java/ru/job4j/calculator/package-info.java
@@ -0,0 +1,9 @@
+/**
+* Package for calculate task.
+*
+* @author Alexey Bogutsky
+* @version  0.1
+* @since 11.02.2019
+*/
+package ru.job4j.calculator;
+ 
