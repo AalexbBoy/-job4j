d59d1377d2dcf61ac83f4819860f53fef4eec489
alex.boy <lemonchic1@gmail.com>
2019-02-26 12:23:32 +0300

    6.1. Классический поиск перебором.[#100493]

---
+++ chapter_001/src/main/java/ru/job4j/array/Turn.java
@@ -0,0 +1,21 @@
+package ru.job4j.array;
+
+/**
+ * Массив задом наперед
+ *
+ * @author Alex.Boy
+ */
+public class Turn {
+    /**
+     * @param array входной массив
+     * @return перевернутый массив
+     */
+    public int[] back(int[] array) {
+        for (int i = 0; i < array.length / 2; i++) {
+            int tmp = array[i];
+            array[i] = array[array.length - i - 1];
+            array[array.length - i - 1] = tmp;
+        }
+        return array;
+    }
+}
