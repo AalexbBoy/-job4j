7267d8abb5ee48c866e2905bdf1540dbc80d4785
AalexBboy <Pumba135a>
2021-02-10 16:09:41 +0300

    Крестики-нолики на JavaFX

--- chapter_004/src/main/java/ru/job4j/stream/Analyze.java
+++ chapter_004/src/main/java/ru/job4j/stream/Analyze.java
@@ -26,10 +26,9 @@ public class Analyze {
                 .orElse(0.0))
         ).collect(Collectors.toList());
     }
-
     public static List<Tuple> averageScoreByPupil(Stream<Pupil> stream) {
         return stream
-                .flatMap(x -> x.getSubjects()
+                .flatMap(pupil -> pupil.getSubjects()
                         .stream())
                 .collect(Collectors.groupingBy(Subject::getName, LinkedHashMap::new, Collectors.averagingDouble(Subject::getScore)))
                 .entrySet()
@@ -39,15 +38,15 @@ public class Analyze {
     }
 
     public static Tuple bestStudent(Stream<Pupil> stream) {
-        return stream.map(e -> new Tuple(e.getName(), (e.getSubjects().stream())
+        return stream.map(pupil -> new Tuple(pupil.getName(), (pupil.getSubjects().stream())
                 .mapToInt(Subject::getScore).sum()))
                 .max(Comparator.comparing(Tuple::getScore))
                 .orElse(null);
     }
 
     public static Tuple bestSubject(Stream<Pupil> stream) {
-        return stream.flatMap(x -> x.getSubjects().stream())
-                .collect(Collectors.groupingBy(Subject::getName, LinkedHashMap::new, Collectors.summingDouble(Subject::getScore)))
+        return stream.flatMap(pupil -> pupil.getSubjects().stream())
+                .collect(Collectors.groupingBy(Subject::getName,  Collectors.summingDouble(Subject::getScore)))
                 .entrySet()
                 .stream()
                 .map(s -> new Tuple(s.getKey(), s.getValue()))
